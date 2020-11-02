f1d3759ba05311ec1264cf55c176924f319a64a9
Nemo-ElAlaily <tassnim.elalaily@gmail.com>
2020-11-02 10:43:03 +0200

    owl.carousel added and new product section is working normally

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,19 @@ $('.dropdown-global-search a').click(function(){
     $('input[name="search-value"]').attr('placeholder', $(this).html());
     $('#globalSearchMenuButton').html($(this).data("target"));
 });
+
+
+// carousel infinite loop - New products card
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
+
+var owl = $('.owl-carousel');
+    owl.owlCarousel({
+    items: 5,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true
+});
