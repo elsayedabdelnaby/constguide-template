eb730106053dae2812c27ac287129c58839560f5
Nemo-ElAlaily <tassnim.elalaily@gmail.com>
2020-11-02 11:39:37 +0200

    owl.carousel added and new product section is working normally0

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 "use strict";
 
-$('.dropdown-global-search a').click(function(){
-    $('input[name="search-module"]').val($(this).data("target"));
-    $('input[name="search-value"]').attr('placeholder', $(this).html());
-    $('#globalSearchMenuButton').html($(this).data("target"));
+$('.dropdown-global-search a').click(function () {
+  $('input[name="search-module"]').val($(this).data("target"));
+  $('input[name="search-value"]').attr('placeholder', $(this).html());
+  $('#globalSearchMenuButton').html($(this).data("target"));
 });
 
 
@@ -13,11 +13,30 @@ $(document).ready(function () {
 });
 
 var owl = $('.owl-carousel');
-    owl.owlCarousel({
-    items: 5,
-    loop: true,
-    margin: 10,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true
+owl.owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  autoWidth:true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    480: {
+      items: 2,
+    },
+    768: {
+      items: 3,
+    },
+    992: {
+      items: 4,
+    },
+    1200: {
+      items: 5,
+      margin:20,
+    },
+  }
 });
