c1f50a248a1f1564c0f3e152fe5d19b559e48383
Nemo-ElAlaily <tassnim.elalaily@gmail.com>
2020-11-04 14:55:36 +0200

    Full homepage with responsive and animation done

--- js/custom.js
+++ js/custom.js
@@ -45,6 +45,28 @@ owl.owlCarousel({
 
 $(document).ready(function () {
   $(".owl-carousel-sponser").owlCarousel();
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 5000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
 
 var owl = $('.owl-carousel-sponser');
@@ -74,3 +96,28 @@ owl.owlCarousel({
     },
   }
 });
+
+
+// --------------------------- start Back to top Button --------------------------- 
+//Get the button:
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop =  $('html,body').animate({ scrollTop: 0 }, 'slow');; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTop =  $('html,body').animate({ scrollTop: 0 }, 'slow');; // For Chrome, Firefox, IE and Opera
+}
+
+// --------------------------- end Back to top Button --------------------------- 
